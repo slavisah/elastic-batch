f9926a1ce263f0875a7bacfcd9974c96294247fd
slavisah <shrkac@gmail.com>
2017-12-20 15:59:16 +0100

    first commit

---
+++ src/main/java/io/github/slavisah/elasticbatch/ElasticBatchApplication.java
@@ -0,0 +1,12 @@
+package io.github.slavisah.elasticbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBatchApplication.class, args);
+	}
+}
